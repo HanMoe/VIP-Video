e4225a74c5756b8e7728e18379c59d5ffed94f5b
Childe Chén <chenmingmao@gmail.com>
2019-02-28 12:53:25 +0800

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 function start(){
 	if($('input').val() != ""){
-		var url = "https://api.smq1.com/?url="+$('input').val();
+		var url = "http://www.85105052.com/admin.php?url="+$('input').val();
         layui.use('layer', function(){
 			  layer.open({
 			  type: 2
